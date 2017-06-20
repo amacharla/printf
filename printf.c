c688bcd98d454111568e4a01c0dd6a4619cf1d75
amacharla <noop2016@gmail.com>
2017-06-20 05:04:05 +0000

    fixed non mod issue

--- printf.c
+++ printf.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
-
 /**
 ** _printf - gives output
 ** @format: char
 ** Return: num of chars
 **/
-
 int _printf(const char *format, ...)
 {
 	unsigned int i, j, count;
@@ -19,29 +17,35 @@ int _printf(const char *format, ...)
 	/* initialize va list and transvers and count */
 	va_start(arguments, format);
 	i = 0, j = 0, count = 0;
+	
 	while (format && format[i])
 	{
 		if (format[i] == '%' && format[i + 1])
 		{
 			mod = format[i + 1];
 			while (print_func[j].type)
-			{	/* go through type till it matches format character */
+			{
 				if (print_func[j].type == mod)
 				{
 					count += print_func[j].printer(arguments);
 					break;
 				}
-				j++; /*transverse through type*/
+				j++;
+			}
+			if (print_func[j].type == '\0')
+			{
+				_putchar('%'); _putchar(mod);
+				count += 2; i++; /*move past %*/
 			}
 			j = 0; /*reset transverse for type if matched or hits null*/
-			i++;
+			i++; /*move past mod*/
 		}
 		else
 		{
 			_putchar(format[i]);
 			count++;
+			i++; /*move to next argument*/
 		}
-		i++; /*transverse through format*/
 	}
 	va_end(arguments);
 	return (count);
